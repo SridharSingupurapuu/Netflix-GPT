279122d623135a34a87e9a74255a0601e4f7c239
Sridhar <singupurapusridhar570@gmail.com>
2024-05-15 17:19:13 +0530

    environment install and hidden firebase secrets

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCK-VTi5YPFxeiFqATfqUXtPR8Xec-yrDg",
-  authDomain: "netflixgpt-864f4.firebaseapp.com",
-  projectId: "netflixgpt-864f4",
-  storageBucket: "netflixgpt-864f4.appspot.com",
-  messagingSenderId: "582023607118",
-  appId: "1:582023607118:web:81b01008f9a05a3144641f",
-  measurementId: "G-YTR9BRGKFE",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
